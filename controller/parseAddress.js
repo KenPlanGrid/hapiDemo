53299f29f8fc33513259fe33c38cdc9511daca36
Ken Lee <ken.lee@unisonim.com>
2018-04-10 13:47:49 -0700

    test

--- controller/parseAddress.js
+++ controller/parseAddress.js
@@ -1,22 +1,20 @@
 const Joi = require('joi');
+const parser = require('parse-address');
 
 module.exports = {
   method: 'POST',
   path: '/hello33',
   config: {
     handler: (request, h) => {
-      return 'hello world';
+      const parsedAddr = parser.parseLocation(request.payload.address);
+
+      if (!parsedAddr.state || !parsedAddr.city || !parsedAddr.street || !parsedAddr.zip) return request.payload.address;
+
+      return parsedAddr;
     },
     validate: {
       payload: Joi.object({
-        delivery_line_1: Joi.string().required(),
-        city: Joi.string().required(),
-        state: Joi.string().required(),
-        zipcode: Joi.string().required(),
-        product_type: Joi.string(),
-        mortgage_balance: Joi.number().min(0).max(50000000),
-        property_use: Joi.string(),
-        webhook: Joi.string(),
+        address: Joi.string().required(),
       }).unknown(),
     },
   },
